4077721c8a835a98e43fb2a7decdf6da11fe831a
Bela Ban <belaban@yahoo.com>
2014-11-27 14:37:54 +0100

    ns

--- bin/run.bat
+++ bin/run.bat
@@ -24,7 +24,7 @@ set LOG=-Dlog4j.configurationFile=%CONF%\log4j2.xml
 
 set JG_FLAGS=-Djgroups.bind_addr=%BIND_ADDR% -Djgroups.udp.mcast_addr=%MCAST_ADDR%
 set JG_FLAGS=%JG_FLAGS% -Djava.net.preferIPv4Stack=true
-set FLAGS=-server -Xmx600M -Xms600M
+set FLAGS=-Xmx600M -Xms600M
 set FLAGS=%FLAGS% -XX:CompileThreshold=10000 -XX:ThreadStackSize=64K -XX:SurvivorRatio=8
 set FLAGS=%FLAGS% -XX:TargetSurvivorRatio=90 -XX:MaxTenuringThreshold=15
 set FLAGS=%FLAGS% -Xshare:off
--- bin/run.sh
+++ bin/run.sh
@@ -6,6 +6,7 @@
 #BIND_ADDR=192.168.1.5
 #BIND_ADDR=match-interface:en.*
 #BIND_ADDR=site_local
+#BIND_ADDR=127.0.0.1
 
 ################# CHANGE THIS ##############################
 BIND_ADDR=match-address:192.168.1.*
