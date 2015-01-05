57f0f6750f12262779073188cc0ab64c4c2e843e
Bela Ban <belaban@yahoo.com>
2015-01-05 11:58:07 +0100

    ns

--- src/org/lab/protocols/PRINT_BYTES.java
+++ src/org/lab/protocols/PRINT_BYTES.java
@@ -1,9 +1,13 @@
 package org.lab.protocols;
 
 import org.jgroups.Event;
+import org.jgroups.Message;
+import org.jgroups.View;
+import org.jgroups.annotations.Property;
 import org.jgroups.conf.ClassConfigurator;
 import org.jgroups.stack.Protocol;
 import org.jgroups.util.MessageBatch;
+
 
 /**
  * @author Bela Ban
@@ -11,6 +15,9 @@ import org.jgroups.util.MessageBatch;
  */
 public class PRINT_BYTES extends Protocol {
     protected static final short ID=2015;
+
+    @Property(description="Suppresses printing to stdout if false")
+    protected boolean do_print=true;
 
     static {
         ClassConfigurator.addProtocol(ID, PRINT_BYTES.class);
@@ -33,14 +40,45 @@ public class PRINT_BYTES extends Protocol {
     }
 
     public Object down(Event evt) {
-        return super.down(evt);
+        switch(evt.getType()) {
+            case Event.VIEW_CHANGE:
+                View view=(View)evt.getArg();
+                System.out.println("view = " + view);
+                break;
+            case Event.MSG:
+                if(do_print) {
+                    Message msg=(Message)evt.getArg();
+                    int num_bytes=msg.getLength();
+                    if(num_bytes > 0)
+                        System.out.printf("-- sending %d bytes\n", num_bytes);
+                    // System.out.println("headers are " + msg.getHeaders());
+                }
+                break;
+        }
+        return down_prot.down(evt);
     }
 
     public Object up(Event evt) {
-        return super.up(evt);
+        switch(evt.getType()) {
+            case Event.MSG:
+                if(do_print) {
+                    Message msg=(Message)evt.getArg();
+                    int num_bytes=msg.getLength();
+                    if(num_bytes > 0)
+                        System.out.printf("-- received %d bytes\n", num_bytes);
+                }
+                break;
+        }
+        return up_prot.up(evt);
     }
 
     public void up(MessageBatch batch) {
-        super.up(batch);
+        int total_bytes=0;
+        for(Message msg: batch)
+            total_bytes+=msg.getLength();
+        // alternative: total_bytes=batch.length();
+        System.out.printf("received batch of %d messages: total size is %d bytes\n", batch.size(), total_bytes);
+        if(!batch.isEmpty())
+            up_prot.up(batch);
     }
 }
