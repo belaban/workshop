051d4575ed387348c7cbb358b81db7d21785cdb2
Fabio Marinelli <f.marinelli@gmail.com>
2015-06-12 01:30:21 +0200

    Added managed operation to the PRINT_BYTES protocols to register/unregister dynamically the JGroups Channel to JMX

--- src/org/lab/protocols/PRINT_BYTES.java
+++ src/org/lab/protocols/PRINT_BYTES.java
@@ -3,11 +3,14 @@ package org.lab.protocols;
 import org.jgroups.Event;
 import org.jgroups.Message;
 import org.jgroups.View;
+import org.jgroups.annotations.ManagedOperation;
 import org.jgroups.annotations.Property;
 import org.jgroups.conf.ClassConfigurator;
 import org.jgroups.conf.PropertyConverter;
+import org.jgroups.jmx.JmxConfigurator;
 import org.jgroups.stack.Protocol;
 import org.jgroups.util.MessageBatch;
+import org.jgroups.util.Util;
 
 
 /**
@@ -24,6 +27,25 @@ public class PRINT_BYTES extends Protocol {
 
     @Property(description="bla",converter=MyConverter.class)
     protected Flags flags;
+
+    private boolean is_registered = false;
+
+    @ManagedOperation(description = "Register to jmx")
+    public synchronized void register(boolean register) {
+        try {
+            if (register && !is_registered) {
+                JmxConfigurator.registerChannel(this.getProtocolStack().getChannel(), Util.getMBeanServer(), this.getTransport().getClusterName());
+                is_registered = true;
+            } else {
+                if (is_registered) {
+                    JmxConfigurator.unregisterChannel(this.getProtocolStack().getChannel(), Util.getMBeanServer(), this.getTransport().getClusterName());
+                    is_registered = false;
+                }
+            }
+        } catch (Exception e) {
+            log.warn(e.getMessage(), e);
+        }
+    }
 
     public static class MyConverter implements PropertyConverter {
 
